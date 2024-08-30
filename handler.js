f9802c051e5a356c2aed0270a00b059ec2f3350f
Abu Sakib <abu@moesif.com>
2024-08-30 16:39:23 +0600

    Integrate Moesif

--- handler.js
+++ handler.js
@@ -1,8 +1,23 @@
-exports.hello = async (event) => {
-  return {
+require('dotenv').config();
+const moesif = require('moesif-aws-lambda');
+//const middy = require('@middy/core');
+
+const moesifOptions = {
+  application_id: process.env.MOESIF_APPLICATION_ID,
+  logBody: true
+};
+
+
+const hello = async (event, context, callback) => {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
     }),
-  };
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
 };
+
+exports.hello = moesif(moesifOptions, hello);
