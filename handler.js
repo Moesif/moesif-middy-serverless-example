506498a5ac6cc79f073808d3aee6a47029aafbab
Abu Sakib <abu@moesif.com>
2024-09-04 16:04:50 +0600

    Add more middleware and improve the example

--- handler.js
+++ handler.js
@@ -1,31 +1,63 @@
-import moesif from 'moesif-aws-lambda';
-import middy from '@middy/core';
-import inputOutputLogger from '@middy/input-output-logger';
-import httpSecurityHeaders from '@middy/http-security-headers';
-import 'dotenv/config';
+import moesif from "moesif-aws-lambda";
+import middy from "@middy/core";
+import httpSecurityHeaders from "@middy/http-security-headers";
+import validator from "@middy/validator";
+import { transpileSchema } from "@middy/validator/transpile";
+import jsonBodyParser from "@middy/http-json-body-parser";
+
+import "dotenv/config";
+import httpErrorHandler from "@middy/http-error-handler";
 
 const moesifOptions = {
   application_id: process.env.MOESIF_APPLICATION_ID,
-  logBody: true
+  logBody: true,
 };
 
-const hello = async (event, context) => {
+const greetingsHandler = async (event, context) => {
+  const { greetingsMessage, greeterName } = event.body;
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Moesif, Middy, and Serverless! Your function executed successfully!",
+      message: "Greetings details received successfully!",
+      greetingsDetails: {
+        greetingsMessage: greetingsMessage,
+        greeter: greeterName,
+      },
     }),
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
   };
   return response;
 };
 
+// The `validator` middleware uses this schema to validate the event body.
+const schema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        greetingsMessage: {
+          type: "string",
+        },
+        greeterName: {
+          type: "string",
+        },
+      },
+      required: ["greetingsMessage", "greeterName"],
+    },
+  },
+};
+
+// Wrap the final handler from Middy with `moesif`.
 export const lambdaHandler = moesif(
-  moesifOptions, 
+  moesifOptions,
   middy()
     .use(httpSecurityHeaders())
-    .use(inputOutputLogger())
-  .handler(hello)
+    .use(jsonBodyParser())
+    .use(validator({ eventSchema: transpileSchema(schema) }))
+    .use(httpErrorHandler())
+    .handler(greetingsHandler)
 );
